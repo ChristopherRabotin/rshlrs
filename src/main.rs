3253b0ec693f9adf07700a20ba0db64db4d1cf4b
Christopher Rabotin <christopher.rabotin@gmail.com>
2016-12-27 23:16:50 -0700

    Making progress

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,16 @@ fn main(){
     }
 }
 
-fn echo_err(why: std::io::Error, s: &mut BufStream<TcpStream>){
-    let _ = s.write_all(why.description().as_bytes());
-    let _ = s.write_all(b"\n");
+fn echo_err(why: std::io::Error, s: &mut BufStream<TcpStream>)-> std::io::Result<()>{
+    s.write_all(why.description().as_bytes());
+    s.write_all(b"\n")
 }
 
-fn echo_str(str: std::string::String, s: &mut BufStream<TcpStream>){
-    let _ = s.write_all(str.as_bytes());
-    let _ = s.write_all(b"\n");
-    let _ = s.flush();
+fn echo_str(str: std::string::String, s: &mut BufStream<TcpStream>) -> std::io::Result<()>{
+    //s.write_all(str.as_bytes()).and_then(s.write_all(b"\n").and_then(s.flush()));
+    s.write_all(str.as_bytes());
+    s.write_all(b"\n");
+    s.flush()
 }
 
 fn handler(s: &mut BufStream<TcpStream>){
@@ -36,33 +37,54 @@ fn handler(s: &mut BufStream<TcpStream>){
         let _ = s.write(b"> ");
         let _ = s.flush();
         let mut cmd = std::string::String::new();
-        let _ = s.read_line(&mut cmd).unwrap();
+        s.read_line(&mut cmd).unwrap();
         let cmd_splt : Vec<_> = cmd.as_str().split_whitespace().collect();
         let mut args = Vec::new();
         for i in 1..cmd_splt.len() {
             args.push(cmd_splt[i]);
         }
-
-        match Command::new(cmd_splt[0]).args(&args).stdout(Stdio::piped()).spawn() {
-            Err(why) => echo_err(why, s),
-            Ok(process) => {
-                let mut stde = String::new();
-                match process.stderr {
-                    Some(mut err) => {
-                        match err.read_to_string(&mut stde) {
-                            Err(why) => echo_err(why, s),
-                            Ok(_) => echo_str(stde, s),
-                        };
-                    },
-                    None => {
-                        let mut stdo = String::new();
-                        match process.stdout.unwrap().read_to_string(&mut stdo) {
-                            Err(why) => echo_err(why, s),
-                            Ok(_) => echo_str(stdo, s),
+        let main_cmd = cmd_splt[0];
+        // Here goes the command processing.
+        match main_cmd{
+            "exit" => break,
+            _ => match Command::new(main_cmd).args(&args).stdout(Stdio::piped()).spawn() {
+                Err(why) => {
+                    match echo_err(why, s){
+                        Err(why) => println!("could not send error {:?}", why),
+                        Ok(_) =>{}
+                    }
+                },
+                Ok(process) => {
+                    let mut stde = String::new();
+                    match process.stderr {
+                        Some(mut err) => {
+                            match err.read_to_string(&mut stde) {
+                                Err(why) => match echo_err(why, s){
+                                    Err(why) => println!("could not send error {:?}", why),
+                                    Ok(_) =>{}
+                                },
+                                Ok(_) => match echo_str(stde, s){
+                                    Err(why) => println!("{:?}", why),
+                                    Ok(_) =>{}
+                                },
+                            };
+                        },
+                        None => {
+                            let mut stdo = String::new();
+                            match process.stdout.unwrap().read_to_string(&mut stdo) {
+                                Err(why) => match echo_err(why, s){
+                                    Err(why) => println!("could not send error {:?}", why),
+                                    Ok(_) =>{}
+                                },
+                                Ok(_) => match echo_str(stdo, s){
+                                    Err(why) => println!("{:?}", why),
+                                    Ok(_) =>{}
+                                },
+                            }
                         }
                     }
-                }
-            },
-        };
+                },
+            }
+        }
     }
 }
